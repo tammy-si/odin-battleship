b68eefe8af69129e8c4bee386059ffb41a455e02
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-04-11 16:14:50 -0700

    Added another ship test

--- src/ship.test.js
+++ src/ship.test.js
@@ -42,3 +42,23 @@ test('testing to make sure hit works with a ship of length 2', () => {
         isSunk: expect.any(Function)
     ,})
 })
+
+test('testing to make sure hit works with a ship of length 4', () => {
+    let ship = createShip(4);
+    expect(ship.getTimesHit).toBe(0);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.getTimesHit).toBe(3);
+    expect(ship.isSunk()).toBe(false);
+    ship.hit();
+    expect(ship.getTimesHit).toBe(4);
+    expect(ship.isSunk()).toBe(true);
+    expect(ship).toMatchObject({
+        length: 4, 
+        timesHit: 4, 
+        sunk: true,
+        hit: expect.any(Function), 
+        isSunk: expect.any(Function)
+    ,})
+})
