4645a818c52e30919e9d69a7876cdf2ac82d2f33
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-04-12 13:33:29 -0700

    Got some placing ship tests done

--- src/gameboard.js
+++ src/gameboard.js
@@ -14,10 +14,52 @@ function createGameboard() {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ],
 
+        coordsHit: [],
+
         get getBoard() {
             return this.board
         },
 
+        placeShip(init_x, init_y, shipLength, dir) {
+            let shipCoords = this.getShipCoords();
+            // check if every coordinate of the ship would fit in range and is empty
+            if (!(this.checkShipCoords(shipCoords))) {
+                // if the ship doesn't fit we just return false and don't continue placing the ship onto the board
+                return false;
+            }
+        },
+
+        // get ship coords with in the initial
+        getShipCoords(init_x, init_y, shipLength, dir) {
+            let shipCoords = []
+            for (let i = 0; i < shipLength; i++) {
+                // if it's in x direction change the x value
+                if (dir == 'x') {
+                    shipCoords.push([init_x + i, init_y])
+                // y direction, make the ship coordinates go down
+                } else {
+                    shipCoords.push([init_x, init_y + i])
+                }
+            }
+            return shipCoords;
+        },
+
+        // check ship coords
+        checkShipCoords(shipCoords) {
+            for (let i = 0; i < shipCoords.length; i++) {
+                let x = shipCoords[i][0];
+                let y = shipCoords[i][1];
+                if(x < 0 || x > 9 || y < 0 || y > 9 || !(this.checkCoordEmpty(x, y))) {
+                    return false
+                }
+            }
+            return true;
+        },
+
+        // make sure that the coordinate is empty (has a zero, not a ship)
+        checkCoordEmpty(x_coord, y_coord) {
+            return (this.getBoard[x_coord][y_coord] == 0)
+        }
     }
 }
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -14,3 +14,43 @@ test('initilizing the gameboard', ()=> {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
 })
+
+test('test getting potential ship coordinates with a starting 0,0 coord, 4 length, and rot direction x', () => {
+    let board = createGameboard();
+    expect(board.getShipCoords(0, 0, 4, "x")).toEqual([[0, 0], [1, 0], [2, 0], [3, 0]])
+})
+
+test('test getting potential ship coordinates with a starting 0,0 coord, 4 length, and rot direction y', () => {
+    let board = createGameboard();
+    expect(board.getShipCoords(0, 0, 4, "y")).toEqual([[0, 0], [0, 1], [0, 2], [0, 3]])
+})
+
+test('test getting potential ship coordinates with a starting 6,5 coord, 5 length, and rot direction x', () => {
+    let board = createGameboard();
+    expect(board.getShipCoords(6, 5, 5, "x")).toEqual([[6, 5], [7, 5], [8, 5], [9, 5], [10,5]])
+})
+
+test('test getting potential ship coordinates with a starting 5,6 coord, 5 length, and rot direction y', () => {
+    let board = createGameboard();
+    expect(board.getShipCoords(5, 6, 5, "y")).toEqual([[5, 6], [5, 7], [5, 8], [5, 9], [5, 10]])
+})
+
+test('ship coords [[0, 0], [1, 0], [2, 0], [3, 0]] should return true', () => {
+    let board = createGameboard();
+    expect(board.checkShipCoords([[0, 0], [1, 0], [2, 0], [3, 0]])).toBe(true);
+})
+
+test('ship coords [[0, 0], [0, 1], [0, 2], [0, 3]] should return true', () => {
+    let board = createGameboard();
+    expect(board.checkShipCoords([[0, 0], [0, 1], [0, 2], [0, 3]])).toBe(true);
+})
+
+test('ship coords [[6, 5], [7, 5], [8, 5], [9, 5], [10,5]] should return false', () => {
+    let board = createGameboard();
+    expect(board.checkShipCoords([[6, 5], [7, 5], [8, 5], [9, 5], [10,5]])).toBe(false);
+})
+
+test('ship coords [[5, 6], [5, 7], [5, 8], [5, 9], [5, 10]] should return false', () => {
+    let board = createGameboard();
+    expect(board.checkShipCoords([[5, 6], [5, 7], [5, 8], [5, 9], [5, 10]])).toBe(false);
+})
