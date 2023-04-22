924587e810a2aebdf29fb90b711a900535fe7e2c
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-04-22 15:03:24 -0700

    Just about done

--- src/player.test.js
+++ src/player.test.js
@@ -35,15 +35,3 @@ test('testing out a player attacking an enemy board on the same place failing', 
     expect(computer.playersGameboard.getAttacks[0][0]).toEqual(1);
     expect(computer.playersGameboard.getBoard[0][0].getTimesHit).toBe(1);
 });
-
-test('testing out a computer making a random attack', () => {
-    let player = createPlayer();
-    let computer = createPlayer();
-    player.playersGameboard.placeShip(0, 0, 4, 'row');
-    player.playersGameboard.placeShip(1, 0, 3, 'col');
-    computer.playersGameboard.placeShip(0, 0, 4, 'row');
-    computer.playersGameboard.placeShip(1, 0, 3, 'col');
-    expect(player.playersGameboard.getAttackableCoords().length).toBe(100);
-    computer.attackRandom(player);
-    expect(player.playersGameboard.getAttackableCoords().length).toBe(99);
-});
